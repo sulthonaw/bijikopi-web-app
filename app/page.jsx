f36caa48df8f42133a010bba61575635579ea7fb
sulthonaw <sultona412@gmail.com>
2024-11-14 17:03:43 +0700

    fix: update hero

--- app/page.jsx
+++ app/page.jsx
@@ -1,36 +1,42 @@
 import CardProduk from "@/components/cardProduk";
 import Footer from "@/components/footer";
 import NavbarMain from "@/components/navbarMain";
-import { StarIcon } from "@heroicons/react/24/solid";
+import { Button } from "@/components/ui/button";
+import { ArrowDownIcon, StarIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <NavbarMain selected={"produk"} />
-      <section className="container">
-        <div className="mx-auto mt-10 rounded-xl bg-slate-300 bg-[url('/market.jpg')] bg-cover bg-center bg-no-repeat py-28 text-center">
-          <h1 className="my-10 text-4xl font-semibold">Temukan Produkmu</h1>
-          <input
-            id="search"
-            type="text"
-            className="rounded-lg border px-4 py-2 focus:outline-none"
-            placeholder="Pencarian"
-          />
-        </div>
-        <div className="mb-5">
-          <h1>Terdia untukmu</h1>
-        </div>
-        <div className="mb-28 grid grid-cols-6 gap-4">
-          {Array.from({ length: 6 * 5 }).map((_, index) => (
-            <Link key={index} href={"/produk/1"}>
-              <CardProduk />
-            </Link>
-          ))}
+      {/* <NavbarMain selected={"produk"} /> */}
+      <section className="m-2 overflow-hidden rounded-2xl">
+        <div className="relative min-h-[98vh] bg-[url('/coffee-bean.jpg')] bg-cover bg-no-repeat">
+          <div className="relative z-10 flex h-[98vh] items-center justify-center text-white">
+            <div className="max-w-4xl text-center">
+              <h1 className="mb-5 text-9xl font-semibold">BIJIKOPI</h1>
+              <p className="mb-10 text-lg font-medium text-slate-300">
+                From the finest beans straight from local farmers, we bring you an authentic coffee
+                experience.
+              </p>
+              <Button
+                variant={"outline"}
+                className={"h-max gap-5 rounded-full bg-transparent px-6 py-3 text-xl"}
+              >
+                Explore More
+                <div className="textwh aspect-square rounded-full bg-black p-3 text-white">
+                  <ArrowDownIcon width={18} height={18} />
+                </div>
+              </Button>
+            </div>
+          </div>
+          <div className="absolute bottom-0 top-0 w-full bg-gradient-to-t from-black" />
         </div>
       </section>
-      <Footer />
+      <section className="container py-10">
+        <h1>Know more about us</h1>
+      </section>
+      {/* <Footer /> */}
     </>
   );
 }
--- components/navbarMain.jsx
+++ components/navbarMain.jsx
@@ -6,11 +6,35 @@ import { Button } from "./ui/button";
 
 export default function NavbarMain({ selected }) {
   const classSelected = "bg-slate-100 font-semibold";
+
+  const navItems = [
+    {
+      url: "/",
+      name: "Home",
+    },
+    {
+      url: "/catalog",
+      name: "Catalog",
+    },
+    {
+      url: "/faqs",
+      name: "FAQs",
+    },
+    {
+      url: "/about-us",
+      name: "About Us",
+    },
+    {
+      url: "/contact-us",
+      name: "Contact Us",
+    },
+  ];
+
   return (
     <>
-      <Banner />
-      <header className="sticky top-0 z-50 bg-white shadow">
-        <div className="container flex items-center justify-between bg-white py-4">
+      <header className="fixed top-0 z-50 m-4 w-full shadow">
+        <Banner />
+        <div className="container flex items-center justify-between py-4">
           <Link href={"/"}>
             <div className="flex items-center gap-3">
               <Image src={"/logo.png"} width={42} height={42} alt="logo" />
@@ -18,26 +42,18 @@ export default function NavbarMain({ selected }) {
             </div>
           </Link>
           <ul className="bottom-0 flex gap-4">
-            <Link href={"/"}>
-              <li
-                className={
-                  "rounded-lg px-4 py-2 " +
-                  (selected == "produk" ? classSelected : "text-slate-500")
-                }
-              >
-                Produk
-              </li>
-            </Link>
-            <Link href={"/request-produk"}>
-              <li
-                className={
-                  "rounded-lg px-4 py-2 " +
-                  (selected == "request-produk" ? classSelected : "text-slate-500")
-                }
-              >
-                Request Produk
-              </li>
-            </Link>
+            {navItems.map((item, index) => (
+              <Link href={item.url} key={index}>
+                <li
+                  className={
+                    "rounded-lg px-4 py-2 " +
+                    (selected == item.name ? classSelected : "text-slate-500")
+                  }
+                >
+                  {item.name}
+                </li>
+              </Link>
+            ))}
           </ul>
           <div className="flex gap-4">
             <Link href={"/auth/sign-up"}>
